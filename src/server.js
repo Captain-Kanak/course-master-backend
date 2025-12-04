daeefca3f3aa4228c0046882099344e2d0ac2cb2
Captain-Kanak <kanakroy835@gmail.com>
2025-12-04 15:31:23 +0600

    added: config

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import app from "./app.js";
+import config from "./config/index.js";
 
-const port = 5000;
+const port = config.port || 5000;
 
 app.listen(port, () => {
   console.log("Server Listening On Port:", port);
