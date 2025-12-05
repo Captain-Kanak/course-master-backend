020be03869094fe589568fe1e40a6d9a61c80a77
Captain-Kanak <kanakroy835@gmail.com>
2025-12-05 11:54:46 +0600

    fix cors methods for vercel

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ app.use(
       "http://localhost:3000",
     ],
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: "GET,POST,PUT,PATCH,DELETE,OPTIONS",
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
